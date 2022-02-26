f4d3a75097e8c64074bd4da8d6ca1e0eb85ab37e
SeonghakHong <sung3045@naver.com>
2022-02-27 00:20:24 +0900

    concept is changed

--- js/menu.js
+++ js/menu.js
@@ -6,7 +6,7 @@ console.dir(menu[0]);
 
 // 메뉴 클릭 이벤트
 for (i=0; i<menu.length; i++) {
-  // ***메뉴 클릭시 섹션으로 이동
+  // ***메뉴 클릭시 섹션 이동, 이름 활성화
   menu[i].addEventListener('click', function(event){
     // 메뉴 li[0]일시 about 섹션으로 이동
     if (event.target == menu[0]) {
@@ -29,12 +29,8 @@ for (i=0; i<menu.length; i++) {
 
   // ***메뉴 클릭시 해당 섹션의 메뉴이름 active 활성화하기
   menu[i].addEventListener('click', function(event){
-    let getIndexValue = Number(event.target.dataset.index);
-    if (getIndexValue !== i) {
-      event.target.classList.add('active');
-    } else {
-      menu[i].classList.remove('active');
-    }
+    menu[i].classList.remove('active');
+    event.target.classList.add('active');
   });
 }
 
